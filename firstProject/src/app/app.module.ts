d9ab0591d846d8201d9e8db50ba52a9656bdebd3
Mainak Dev <mainak@mainak.local>
2023-02-25 09:15:58 +0530

    feb 25 mid class push

--- firstProject/src/app/app.module.ts
+++ firstProject/src/app/app.module.ts
@@ -4,8 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
-
 import { PostCreateComponent } from './posts/post_create/post-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { PostCreateComponent } from './posts/post_create/post-create.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
