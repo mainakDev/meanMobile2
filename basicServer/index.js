a4e6530699d1a846b7b49b355c156847ea8ac037
Mainak Dev <mainakonandroid@gmail.com>
2023-04-12 11:25:53 +0530

    added basic server

--- basicServer/index.js
+++ basicServer/index.js
@@ -1,18 +1,33 @@
-const http = require('http')
 const express = require('express')
 
 const app = express()
 
 const port = 3001
-const server = http.createServer((req, res) => {
-    res.write('<html>')
-    res.write('<head><title>Node.js Server</title></head>')
-    res.write('<body><h1>First Node.js server</h1></body>')
-    res.write('</html>')
-    res.end()
-    console.log(req)
+//request -> Comes from the front end
+//response -> Comes from the back end
+
+
+//middleware function
+// app.use((req, res, next) => {
+//     console.log('First middleware');
+//     next()
+// })
+// localhost:3001/api/users
+app.use('/api/posts',(req, res, next) => {
+    res.json({
+        message: "Post data",
+        data: [{
+            title: "ABCXYZ",
+            content: 'content'},
+        ]
+    })  
+})
+// localhost:3001/api/products
+app.use('/api/products',(req, res, next) => {
+    res.json({message: "Products data"})  
 })
 
-server.listen(port, () => {
+
+app.listen(port, () => {
     console.log(`Server started at port ${port}`);
 })
